ee6b1236233cc48cebd47c5c454a855fd1d053f1
Filip Paulů <ing.fenix@seznam.cz>
2022-01-15 13:25:20 +0100

    Change initial settings

--- examples/features.ts
+++ examples/features.ts
@@ -5,16 +5,19 @@ const nwv = new NativeWebView(
     {
         title: "Hello title",
         innerSize: { width: 640, height: 420 },
-    },
-    (file) => {
-        const path = resolve(__dirname, file);
-        console.log("File:", file, "->", path);
-        return path;
-    },
-    (message: { type: keyof NativeWebViewSettings } & NativeWebViewSettings[keyof NativeWebViewSettings]) => {
-        console.log("Message from WebView:", message);
-        if (typeof message.type === "string") {
-            nwv.set(message.type, message);
+        getPath: (src) => {
+            const path = resolve(__dirname, src);
+            console.log("Src:", src, "->", path);
+            return path;
+        },
+        onDrop: (drop) => {
+            console.log("Drop:", drop);
+        },
+        onMessage: (message: { type: keyof NativeWebViewSettings } & NativeWebViewSettings[keyof NativeWebViewSettings]) => {
+            console.log("Message from WebView:", message);
+            if (typeof message.type === "string") {
+                nwv.set(message.type, message);
+            }
         }
     }
 );
