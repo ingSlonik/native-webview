5f107d726579c3285ff82e57c814f2f5cf41fd95
Filip Paulů <ing.fenix@seznam.cz>
2022-06-30 00:50:52 +0200

    Update transparent example

--- examples/transparent.ts
+++ examples/transparent.ts
@@ -1,23 +1,18 @@
 import { resolve } from "path";
 import NativeWebView, { NativeWebViewSettings } from "../src/index";
 
-const nwv = new NativeWebView(
-    {
-        title: "Transparent window",
-        transparent: true,
-        innerSize: { width: 420, height: 140 },
-        getPath: (src) => resolve(__dirname, "transparent.html"),
-        onDrop: (drop) => {
-            console.log("Drop:", drop);
-        },
-        onMessage: (message: { type: keyof NativeWebViewSettings } & NativeWebViewSettings[keyof NativeWebViewSettings]) => {
-            console.log("Message from WebView:", message);
-            if (typeof message.type === "string") {
-                nwv.set(message.type, message);
-            }
+const nwv = new NativeWebView({
+    title: "Transparent window",
+    transparent: true,
+    innerSize: { width: 420, height: 150 },
+    getPath: (src) => resolve(__dirname, "transparent.html"),
+    onMessage: (message: { type: keyof NativeWebViewSettings } & NativeWebViewSettings[keyof NativeWebViewSettings]) => {
+        console.log("Message from WebView:", message);
+        if (typeof message.type === "string") {
+            nwv.set(message.type, message);
         }
     }
-);
+});
 
 (async () => {
     await nwv.run();
